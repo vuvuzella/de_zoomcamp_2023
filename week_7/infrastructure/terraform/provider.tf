0569651fe34c128b63b991f42257d346b2e575a0
vuvuzella <j.tabacii@gmail.com>
2023-04-28 00:56:53 +1000

    switched to local backend for terraform

--- week_7/infrastructure/terraform/provider.tf
+++ week_7/infrastructure/terraform/provider.tf
@@ -1,9 +1,11 @@
 terraform {
   required_version = "1.3.6"
-  backend "gcs" {
-    bucket = "202b9fd304fddf3b-bucket-state"
-    prefix = "terraform/project_7"
-  }
+
+  backend "local" {}
+  # backend "gcs" {
+  #   bucket = "202b9fd304fddf3b-bucket-state"
+  #   prefix = "terraform/project_7"
+  # }
   required_providers {
     google = {
       source  = "hashicorp/google"
